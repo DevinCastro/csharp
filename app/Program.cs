d2a24d3a99b380ebcc0667ba0e8042b62c570e20
DevinCastro <dcrocker21@gmail.com>
2020-10-26 21:28:38 -0700

    Calculator app

--- app/Program.cs
+++ app/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,36 @@ namespace app
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World");
+            Console.WriteLine("This is a calculator");
+            Console.WriteLine("Enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What operation do you want to perform? (+,-,*,/)");
+            string op = Console.ReadLine();
+            Console.WriteLine("Enter a second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(op);
+
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine(num1 + num2);
+                    break;
+                case "-":
+                    Console.WriteLine(num1 - num2);
+                    break;
+               case "*":
+                    Console.WriteLine(num1 * num2);
+                    break;
+               case "/":
+                    Console.WriteLine(num1 / num2);
+                    break;
+                default:
+                    // code block
+                    break;
+            }
+
+
             Console.ReadLine();
 
         }
